7c455a56089c84f445bb017793b638b82b82a70e
Bowen-sdu <57526757+Bowen-sdu@users.noreply.github.com>
2025-04-09 19:37:26 +0800

    Update r2r_b16.sh

--- scripts/r2r_b16.sh
+++ scripts/r2r_b16.sh
@@ -9,7 +9,6 @@ obj_ft_dim=0
 
 ngpus=4
 bs=8
-seed=0
 
 object_dim=512
 name=xxx
@@ -35,7 +34,7 @@ flag="--root_dir ${DATA_ROOT}
       --num_x_layers 4
       --num_pano_layers 2
       
-      --max_action_len 15
+      --max_action_len 20
       --max_instr_len 200
 
       --batch_size ${bs}
