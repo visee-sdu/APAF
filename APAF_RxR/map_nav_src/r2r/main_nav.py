d84ada2758c35c6db63f8f5477d25cdac054bd4e
Bowen-sdu <57526757+Bowen-sdu@users.noreply.github.com>
2025-04-09 19:52:32 +0800

    Update main_nav.py

--- APAF_RxR/map_nav_src/r2r/main_nav.py
+++ APAF_RxR/map_nav_src/r2r/main_nav.py
@@ -300,6 +300,7 @@ def main():
     else:
         rank = 0
 
+    args.seed = int(time.time())
     set_random_seed(args.seed + rank)
     train_env, val_envs, aug_env = build_dataset(args, rank=rank, is_test=args.test)
 
